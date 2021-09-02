87307c2387ce6b0e877524a9c51a3ad9ae889ced
Dmitry Maksyoma <ledestin@gmail.com>
2021-09-02 23:20:59 +1200

    Use libjpeg-turbo only in Ubuntu

--- src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
+++ src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
@@ -17,8 +17,10 @@ then
     BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/6f2805e18652781213be6a385f7a05f3d22b218b/kasmvncserver_kali-rolling_0.9.3_master_6f2805_amd64.deb"
 elif [ "${DISTRO}" == "centos" ] ; then
     BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/6f2805e18652781213be6a385f7a05f3d22b218b/kasmvncserver_centos_core_0.9.3_master_6f2805_x86_64.rpm"
-else
+elif [ "$DISTRO" == "ubuntu" ]; then
     BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/dbc376940f971d62451c219f3a354bf6639e7595/kasmvncserver_bionic_0.9.3_feature_KASM-1838-libjpeg-turbo-from-source_dbc376__libjpeg-turbo-latest_amd64.deb"
+else
+    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/6f2805e18652781213be6a385f7a05f3d22b218b/kasmvncserver_bionic_0.9.3_master_6f2805_amd64.deb"
 fi
 
 
