cc406645706233a76acb10835257c79a24ebe501
Matthew McClaskey <matt@kasmweb.com>
2021-09-10 18:37:31 +0000

    Update KasmVNC Build

--- src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
+++ src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
@@ -6,11 +6,11 @@ cd /tmp
 
 if [ "${DISTRO}" == "kali" ]  ;
 then
-    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/0cb2c0ba9f86c45a451e93548c99a45f1cd4b5c8/kasmvncserver_kali-rolling_0.9.3_master_0cb2c0_amd64.deb"
+    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/159d7527955f131e096cf1602b7f9f66cc5d66cb/kasmvncserver_kali-rolling_0.9.3_master_159d75_amd64.deb"
 elif [ "${DISTRO}" == "centos" ] ; then
-    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/0cb2c0ba9f86c45a451e93548c99a45f1cd4b5c8/output/centos_core/kasmvncserver-0.9.1~beta-1.el7.x86_64.rpm"
+    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/159d7527955f131e096cf1602b7f9f66cc5d66cb/output/centos_core/kasmvncserver-0.9.1~beta-1.el7.x86_64.rpm"
 else
-    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/0cb2c0ba9f86c45a451e93548c99a45f1cd4b5c8/kasmvncserver_bionic_0.9.3_master_0cb2c0_amd64.deb"
+    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/159d7527955f131e096cf1602b7f9f66cc5d66cb/kasmvncserver_bionic_0.9.3_master_159d75_amd64.deb"
 fi
 
 
