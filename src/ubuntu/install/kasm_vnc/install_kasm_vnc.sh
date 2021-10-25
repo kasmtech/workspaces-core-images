fd91cbc9c219415f8f26b77b2fcfba72b43a5147
Matthew McClaskey <matt@kasmweb.com>
2021-10-25 11:41:47 +0000

    KASM-1838 Update to latest kasmvnc

--- src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
+++ src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
@@ -4,7 +4,7 @@ set -e
 install_libjpeg_turbo() {
     local libjpeg_deb=libjpeg-turbo.deb
 
-    wget 'https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/dbc376940f971d62451c219f3a354bf6639e7595/output/bionic/libjpeg-turbo_2.1.2_amd64.deb' -O "$libjpeg_deb"
+    wget 'https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/a9434a300dbf85e65d0e9c212610a487fd10a308/output/bionic/libjpeg-turbo_2.1.2_amd64.deb' -O "$libjpeg_deb"
     apt-get install -y "./$libjpeg_deb"
     rm "$libjpeg_deb"
 }
@@ -14,13 +14,11 @@ cd /tmp
 
 if [ "${DISTRO}" == "kali" ]  ;
 then
-    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/d9ceb72c2e982772f5827bb9b9325ff8ad73be5a/kasmvncserver_kali-rolling_0.9.3_feature_KASM-1913_webp_tweaks_d9ceb7_amd64.deb"
+    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/a9434a300dbf85e65d0e9c212610a487fd10a308/kasmvncserver_kali-rolling_0.9.3_master_a9434a_amd64.deb"
 elif [ "${DISTRO}" == "centos" ] ; then
-    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/d9ceb72c2e982772f5827bb9b9325ff8ad73be5a/output/centos_core/kasmvncserver-0.9.1~beta-1.el7.x86_64.rpm"
-elif [ "$DISTRO" == "ubuntu" ]; then
-    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/d9ceb72c2e982772f5827bb9b9325ff8ad73be5a/kasmvncserver_bionic_0.9.3_feature_KASM-1913_webp_tweaks_d9ceb7_libjpeg-turbo-latest_amd64.deb"
+    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/a9434a300dbf85e65d0e9c212610a487fd10a308/output/centos_core/kasmvncserver-0.9.1~beta-1.el7.x86_64.rpm"
 else
-    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/d9ceb72c2e982772f5827bb9b9325ff8ad73be5a/kasmvncserver_bionic_0.9.3_feature_KASM-1913_webp_tweaks_d9ceb7_amd64.deb"
+    BUILD_URL="https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/a9434a300dbf85e65d0e9c212610a487fd10a308/kasmvncserver_bionic_0.9.3_master_a9434a_libjpeg-turbo-latest_amd64.deb"
 fi
 
 
