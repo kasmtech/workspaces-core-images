287a7a0d5d59e8f4b26238a562e91221a0435ba3
matt <matt@kasmweb.com>
2021-12-30 17:05:20 +0000

    KASM-2184 multi-gpu support

--- src/common/xfce/window_manager_startup.sh
+++ src/common/xfce/window_manager_startup.sh
@@ -5,9 +5,9 @@ echo -e "\n------------------ Xfce4 window manager startup------------------"
 
 if [ "${START_XFCE4}" == "1" ] ;
     then
-        if [ -f /usr/bin/vglrun ] && [ -d /dev/dri ] && [ -O /dev/dri/card0 ] && [ -O /dev/dri/renderD128 ] ; then
+        if [ -f /usr/bin/vglrun ] && [ ! -z "${KASM_EGL_CARD}" ] && [ ! -z "${KASM_RENDERD}" ] && [ -O "${KASM_RENDERD}" ] && [ -O "${KASM_EGL_CARD}" ] ; then
 	    echo "Starting XFCE with VirtualGL"
-	    DISPLAY=:1 /usr/bin/vglrun -d /dev/dri/card0 /usr/bin/startxfce4 --replace &
+	    DISPLAY=:1 /usr/bin/vglrun -d "${KASM_EGL_CARD}" /usr/bin/startxfce4 --replace &
 	else
             echo "Starting XFCE"
             /usr/bin/startxfce4 --replace &
