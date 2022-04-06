b55701d4dd6ddb4f4715691a8caaef00851f8f1e
Ryan Kuba <ryan.kuba@kasmweb.com>
2022-04-06 17:15:22 +0000

    Resolve KASM-1423 "Bugfix/ pulseaudio container storage"

--- src/ubuntu/install/audio/install_audio.sh
+++ src/ubuntu/install/audio/install_audio.sh
@@ -28,6 +28,8 @@ else
   apt-get install -y curl git ffmpeg
 fi
 
+mkdir -p /var/run/pulse
+
 cd $STARTUPDIR
 mkdir jsmpeg
 wget -qO- https://kasmweb-build-artifacts.s3.amazonaws.com/kasm_websocket_relay/f173f72a9faa6239e43f2efcb48aabe8a984d443/kasm_websocket_relay_${DISTRO/kali/ubuntu}_${ARCH}_develop.f173f7.tar.gz | tar xz --strip 1 -C $STARTUPDIR/jsmpeg
