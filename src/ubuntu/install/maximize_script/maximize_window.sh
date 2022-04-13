9f910916cca1f653f3021c3bfd1ebc25b0cb760d
Richard Koliser <richard.koliser@kasmweb.com>
2022-04-13 18:25:50 -0400

    KASM-2393 Silence logging in the maximize loop
    the maximize script doesn't set -x itself, but since we loop inside, and
    -x may be set by the calling script, this will reduce the verbosity of
    the logs. After we exit we restore -x if it was previously set.
    
    Removed an echo that would happen in the loop.

--- src/ubuntu/install/maximize_script/maximize_window.sh
+++ src/ubuntu/install/maximize_script/maximize_window.sh
@@ -1,16 +1,21 @@
 # MAXIMIZE and MAXIMIZE_NAME are exported by the calling script
 # This script will check against the container OS to do the right thing.
+enable_x=0
 
 maximize_window(){
     set +e
     if [[ ${MAXIMIZE} == 'true' ]] ; then
+        if [[ $- =~ x ]] ;
+        then
+            set +x
+            enable_x=1
+        fi
         while true; do
             end=$((SECONDS+60))
             while [ $SECONDS -lt $end ]; do
                 windows=$(wmctrl -l)
                 if [[ "$windows" =~ "${MAXIMIZE_NAME}" ]];
                 then
-                    echo "Found ${MAXIMIZE_NAME}, maximizing"
                     wmctrl -r "${MAXIMIZE_NAME}" -b add,maximized_vert,maximized_horz
                     break;
                 fi
@@ -18,6 +23,10 @@ maximize_window(){
             done
             sleep 10
         done
+        if [[ ${enable_x} -eq 1 ]];
+        then
+            set -x
+        fi
     fi
     set -e
 }
