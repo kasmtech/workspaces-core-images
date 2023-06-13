ceb023805ac73aa43b790be79064bcf2deaaf617
Mariusz Marciniak <mariusz.marciniak.pl@gmail.com>
2023-06-13 12:28:37 +0200

    KASM-4083 Updated kasm-webcam-server version

--- src/ubuntu/install/webcam/install_webcam.sh
+++ src/ubuntu/install/webcam/install_webcam.sh
@@ -8,7 +8,7 @@ elif [ "${DISTRO}" == "oracle8" ]; then
   DISTRO=oracle
 fi
 
-COMMIT_ID="51d8d626581873be96861827829281b8f71fddb0"
+COMMIT_ID="1631ff2c5f79ff6892c1d357343c47cb6f7c7a29"
 BRANCH="develop"
 COMMIT_ID_SHORT=$(echo "${COMMIT_ID}" | cut -c1-6)
 
@@ -17,3 +17,4 @@ ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
 mkdir -p $STARTUPDIR/webcam
 wget -qO- https://kasmweb-build-artifacts.s3.amazonaws.com/kasm_webcam_server/${COMMIT_ID}/kasm_webcam_server_${ARCH}_${BRANCH}.${COMMIT_ID_SHORT}.tar.gz | tar -xvz -C $STARTUPDIR/webcam/
 
+echo "${BRANCH}:${COMMIT_ID}" > $STARTUPDIR/webcam/kasm_webcam_server.version
