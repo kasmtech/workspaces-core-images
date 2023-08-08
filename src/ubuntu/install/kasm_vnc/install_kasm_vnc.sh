b875f60e83a7343d34ec07df55eef706eb030af2
Mariusz Marciniak <mariusz.marciniak.pl@gmail.com>
2023-08-08 16:27:36 +0200

    KASM-4608 Point KasmVNC to the release branch

--- src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
+++ src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
@@ -17,8 +17,8 @@ echo "Install KasmVNC server"
 cd /tmp
 BUILD_ARCH=$(uname -p)
 UBUNTU_CODENAME=""
-COMMIT_ID="d0db55dcbc5e5d02f0102f7beb841861a00533c6"
-BRANCH="feature_KASM-4608_linux_printing" # just use 'release' for a release branch
+COMMIT_ID="fa884809145e0ba6e5e1da49887a82b5cab0a1d1"
+BRANCH="release" # just use 'release' for a release branch
 KASMVNC_VER="1.1.1"
 COMMIT_ID_SHORT=$(echo "${COMMIT_ID}" | cut -c1-6)
 
