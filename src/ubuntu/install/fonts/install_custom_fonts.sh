3290c8dd87872fa4a2af59b3c6dfdcf325a3f53f
Teja Swaroop Pothala <tejaswaroop.pothala@kasmweb.com>
2025-01-15 20:19:06 +0530

    modify custom fonts url for opensuse-15 in 1.16.1

--- src/ubuntu/install/fonts/install_custom_fonts.sh
+++ src/ubuntu/install/fonts/install_custom_fonts.sh
@@ -44,7 +44,7 @@ elif [[ "${DISTRO}" == @(oracle8|oracle9|rhel9|rockylinux9|rockylinux8|almalinux
   done
 elif [ "${DISTRO}" == "opensuse" ]; then
   zypper addrepo -G \
-    https://download.opensuse.org/repositories/M17N:/fonts/15.5/ fonts-x86_64
+    https://download.opensuse.org/repositories/M17N:/fonts/15.6/ fonts-x86_64
   zypper install -ny \
     glibc-i18ndata \
     glibc-locale \
