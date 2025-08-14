8d849e2ff9ef9fa29676b737e090fb8baa6d5ff2
Huan Truong <huan.truong@kasmweb.com>
2025-08-14 14:03:44 -0500

    QA-136 updated rules logic

--- ci-scripts/weekly-manifest.sh
+++ ci-scripts/weekly-manifest.sh
@@ -48,9 +48,9 @@ if [[ "${TYPE}" == "multi" ]]; then
   
 # Single arch image just pull and push
 else
-    tagImage "${ORG_NAME}/${NAME}:x86_64-${SANITIZED_BRANCH_DAILY}" "${ORG_NAME}/${NAME}:${SANITIZED_BRANCH}"
+    tagImage "${ORG_NAME}/${NAME}:${SANITIZED_BRANCH_DAILY}" "${ORG_NAME}/${NAME}:${SANITIZED_BRANCH}"
     
     for MIRROR in "${REGISTRY_MIRRORS[@]}"; do
-        tagImage "${ORG_NAME}/${NAME}:x86_64-${SANITIZED_BRANCH_DAILY}" "${MIRROR}/${MIRROR_ORG_NAME}/${NAME}:${SANITIZED_BRANCH}"
+        tagImage "${ORG_NAME}/${NAME}:${SANITIZED_BRANCH_DAILY}" "${MIRROR}/${MIRROR_ORG_NAME}/${NAME}:${SANITIZED_BRANCH}"
     done
 fi
